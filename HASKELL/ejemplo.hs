ec6092aaf13c713243d41aff0bc85c4df0b7e77e
profe Joel <joel.torres@ulagos.cl>
2021-06-09 10:36:56 -0400

    comienzo en Haskell

---
+++ HASKELL/ejemplo.hs
@@ -0,0 +1,14 @@
+import Data.List
+import System.IO
+
+-- Esto es un comentario en una linea
+{- Comentario
+en 
+multiples
+lineas
+-}
+
+always5 :: Int -- Esta definicion de una constante
+always5 = 5
+
+sumOfVals = sum[1..100]
---
+++ HASKELL/ejemplo1.hs
@@ -0,0 +1,35 @@
+--TIpos de datos
+-- Integer Int
+-- Float
+-- Double
+-- Boolean Bool
+-- Char
+-- Tuple -> es una lista de diferentes valores y son inamovibles o inmutables
+
+suma = 5 + 4
+resta = 5 - 4
+multi = 5 * 4
+division = 5 / 4
+division2 = 5 `div` 4
+
+modulo = mod 5 4
+modulo2 = 5 `mod` 4
+
+sumacompleja = 5 + (-4)
+
+
+numero = 16 :: Int
+sqrt_numero = sqrt(fromIntegral numero)
+
+valor_pi = pi
+exponente = exp 3
+logaritmo = log 3
+cuadrado = 3**2
+truncar = truncate 1.56
+redondear = round 1.56
+redondear_hacia_arriba = ceiling 1.56
+redondear_hacia_abajo = floor 1.56
+
+yLogico = True && False
+oLogico = True || False
+noLogico = not(True)
